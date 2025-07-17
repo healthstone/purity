ba923c22dc3505969cb6e7c5769ab00352ebabe7
Владимиров Александр <kvipka90@yandex.ru>
2025-07-17 10:17:00 +0300

    implement both methods : classic bncs and bncs with fixed length packets

--- src/server/SessionMode/bncs/reader/ReaderBNCS.hpp
+++ src/server/SessionMode/bncs/reader/ReaderBNCS.hpp
@@ -1,6 +1,13 @@
 #include "src/server/ClientSession/ClientSession.hpp"
+#include "src/server/SessionMode/bncs/opcodes/opcodes8.hpp"
 
 namespace ReaderBNCS {
 
     void process_read_buffer_as_bncs(std::shared_ptr<ClientSession> session);
+
+    void process_valid_packet(std::shared_ptr<ClientSession> session,
+                                          BNETOpcode8 id,
+                                          const std::vector<uint8_t>& body);
+
+    size_t get_packet_expected_size(BNETOpcode8 id);
 }
